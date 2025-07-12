f0a3410288e4e0bd3324ab93b6d292ebc20fce64
Pratik Ravariya <pratikr0810@gmail.com>
2025-07-12 18:14:50 +0530

    S3

--- next.config.mjs
+++ next.config.mjs
@@ -5,7 +5,8 @@ const nextConfig = {
       { protocol: "https", hostname: "replicate.delivery" },
       { protocol: "https", hostname: "utfs.io" },
       { protocol: "https", hostname: "cdn.shopify.com" },
-      { protocol: "https", hostname: "d7dum6r51r1fd.cloudfront.net" },
+      { protocol: "https", hostname: "d7dum6r51r1fd.cloudfront.net" }, // mybranzs3
+      { protocol: "https", hostname: "d1kw46qh6t9dlp.cloudfront.net" }, // lazedit s3
     ],
   },
   reactStrictMode: true,
