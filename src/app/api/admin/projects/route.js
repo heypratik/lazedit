ac35f44b9064816c013b3170acd73fb6cf617202
Pratik Ravariya <pratikr0810@gmail.com>
2025-10-04 21:55:43 +0530

    fixes

--- src/app/api/admin/projects/route.js
+++ src/app/api/admin/projects/route.js
@@ -3,7 +3,7 @@ import { db } from "../../../../db/drizzle";
 import { projects } from "../../../../db/schema";
 import { eq } from "drizzle-orm";
 
-export const dynamic = "force-dynamic";
+export const dynamic = "force-no-store";
 
 export async function GET(request) {
   try {
