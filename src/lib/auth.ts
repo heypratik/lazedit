626f0354e9223ed67369ac349c2c510c1cad0c7e
Pratik Ravariya <pratikr0810@gmail.com>
2025-10-04 11:43:34 +0530

    Social Provider

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -9,5 +9,12 @@ export const auth = betterAuth({
   emailAndPassword: {
     enabled: true,
   },
+  socialProviders: {
+    google: {
+      enabled: true,
+      clientId: process.env.GOOGLE_CLIENT_ID || "",
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET || "",
+    },
+  },
   secret: process.env.BETTER_AUTH_SECRET || process.env.NEXT_PUBLIC_SECRET,
 });
