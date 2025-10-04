3f870b302164b78f994b37cc6d0044375f31b8ac
Pratik Ravariya <pratikr0810@gmail.com>
2025-10-04 20:15:10 +0530

    Fixes

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
   schema: "./src/db/schema.ts",
   dialect: "postgresql",
   dbCredentials: {
-    url: process.env.NEXT_PUBLIC_POSTGRESS!,
+    url: process.env.DATABASE_URL!,
   },
   verbose: true,
   strict: true,
