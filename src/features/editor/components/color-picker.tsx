f56b58d5477ef1893a73ee4337ba53dc76d3479f
Pratik Ravariya <pratikr0810@gmail.com>
2025-10-04 19:39:12 +0530

    Fixes

--- src/features/editor/components/color-picker.tsx
+++ src/features/editor/components/color-picker.tsx
@@ -1,4 +1,4 @@
-import {ChromePicker, CirclePicker} from 'react-color';
+import { ChromePicker, CirclePicker } from 'react-color';
 import { colors } from '../types';
 import { rgbaObjectToString } from '../utils';
 
@@ -7,7 +7,7 @@ interface ColorPickerProps {
     onChange: (value: string) => void;
 }
 
-export const ColorPicker = ({value, onChange}: ColorPickerProps) => {
+export const ColorPicker = ({ value, onChange }: ColorPickerProps) => {
     return (
         <div className='flex flex-col space-y-4'>
             {/* <ChromePicker color={value} onChange={(color) => {
@@ -15,16 +15,15 @@ export const ColorPicker = ({value, onChange}: ColorPickerProps) => {
                 onChange(formattedValue);
             }} className='border rounded-lg !w-full' /> */}
             <div className="flex items-center gap-2">
-                                    <input 
-                            type="color" 
-                            defaultValue="#ffffff"
-                            className="w-8 h-8 rounded border border-white/20 bg-transparent cursor-pointer"
-                            value={value}
-                            onChange={(e) => onChange(e.target.value)}
-                        />
-                        <span className="text-xs text-white/60">Select color</span>
-                    </div>
-{/* 
+                <input
+                    type="color"
+                    className="w-8 h-8 rounded border border-white/20 bg-transparent cursor-pointer"
+                    value={value}
+                    onChange={(e) => onChange(e.target.value)}
+                />
+                <span className="text-xs text-white/60">Select color</span>
+            </div>
+            {/* 
             <CirclePicker colors={colors} color={value} className='!w-full mt-4' onChangeComplete={(color) => {
                 const formattedValue = rgbaObjectToString(color.rgb);
                 onChange(formattedValue);
