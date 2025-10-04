c3f623eef46a5fbd4e0f961b42dc24600af42eeb
Pratik Ravariya <pratikr0810@gmail.com>
2025-10-04 19:13:44 +0530

    Fix

--- src/app/api/[[...route]]/images.ts
+++ src/app/api/[[...route]]/images.ts
@@ -1,26 +1,15 @@
-import {Hono} from 'hono';
-import { unsplash } from '@/lib/unsplash';
+import { Hono } from 'hono';
 
 const DEFAULT_COUNT = 50
 const DEFAULT_COLLECTION = ["317099"]
 
 const app = new Hono()
-   .get("/", async (c) => {
+    .get("/", async (c) => {
 
-    const images = await unsplash.photos.getRandom({count: DEFAULT_COUNT, collectionIds: DEFAULT_COLLECTION})
+        const response: any[] = []
 
-    if (images.errors) {
-        return c.json({error: "Failed to fetch images"}, 400)
-    }
-
-    let response = images.response
-
-    if (!Array.isArray(response)) {
-        response = [response]
-    }
-
-         return c.json({data: response })
-   })
+        return c.json({ data: response })
+    })
 
 
 export default app
